88277b01aefdb09cd837fcef6062ca2722bc6e01
IWontRememberThisName <tkd2012tkd@gmail.com>
2026-01-31 18:06:24 -0800

    Controls for Mask implemented
    Pressing shift puts the mask on, but the functionality relating to the NPC characters is still WIP

--- Assets/Scripts/Shawn's Scripts/PlayerController.cs
+++ Assets/Scripts/Shawn's Scripts/PlayerController.cs
@@ -7,18 +7,31 @@ public class PlayerController : MonoBehaviour
     public float speed = 10f;
     public Vector3 direction;
     private Rigidbody2D rb;
+
+    bool maskEquipped = false;
+
+    public Renderer rend;
+
+    public Color originalColor;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalColor = rend.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        MaskOn();
+        MaskOff();
     }
 
+    /// <summary>
+    /// Handles the movement of the player with WASD controls
+    /// </summary>
     private void Movement()
     {
         Vector3 pos = transform.position;
@@ -41,4 +54,36 @@ public class PlayerController : MonoBehaviour
         }
         transform.position = pos;
     }
+
+    /// <summary>
+    /// Controls the activation of the mask
+    /// </summary>
+    public void MaskOn()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!maskEquipped)
+            {
+                maskEquipped = true;
+                rend.material.color = Color.red;
+                print("test");
+            }
+        }
     }
+    /// <summary>
+    /// Controls the deactivation of the mask
+    /// </summary>
+    public void MaskOff()
+    {
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (maskEquipped)
+            {
+                maskEquipped = false;
+                rend.material.color = originalColor;
+            }
+        }
+    }
+
+ }
